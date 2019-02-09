70fa590847d4ae8e1e4644ee6aecc23fd24583a4
root <jeff@osstechnolab.com>
2019-02-09 14:47:24 +0000

    \ 	modified:   shoebox/custom_scripts/journal_entry.js 	modified:   shoebox/shoebox/custom/supplier.json 	modified:   shoebox/shoebox/doctype/payment_transaction/payment_transaction.json 	modified:   shoebox/shoebox/doctype/purchase_transaction/purchase_transaction.json 	modified:   shoebox/shoebox/doctype/receipt_transaction/receipt_transaction.json 	modified:   shoebox/shoebox/doctype/sales_transaction/sales_transaction.json

--- shoebox/custom_scripts/journal_entry.js
+++ shoebox/custom_scripts/journal_entry.js
@@ -23,6 +23,9 @@ frappe.ui.form.on("Journal Entry", {
 								() => frm.set_value("tin", trans.tin),
 								() => frm.set_value("address_1", trans.address_1),
 								() => frm.set_value("address_2", trans.address_2),
+								() => frm.set_value("first_name", trans.first_name),
+								() => frm.set_value("middle_name", trans.middle_name),
+								() => frm.set_value("last_name", trans.last_name),
 								() => done()
 							]);
 						}
@@ -40,6 +43,9 @@ frappe.ui.form.on("Journal Entry", {
 								() => frm.set_value("tin", trans.tin),
 								() => frm.set_value("address_1", trans.address_1),
 								() => frm.set_value("address_2", trans.address_2),
+								() => frm.set_value("first_name", trans.first_name),
+								() => frm.set_value("middle_name", trans.middle_name),
+								() => frm.set_value("last_name", trans.last_name),
 								() => done()
 							]);
 						}
@@ -57,6 +63,9 @@ frappe.ui.form.on("Journal Entry", {
 								() => frm.set_value("tin", trans.tin),
 								() => frm.set_value("address_1", trans.address_1),
 								() => frm.set_value("address_2", trans.address_2),
+								() => frm.set_value("first_name", trans.first_name),
+								() => frm.set_value("middle_name", trans.middle_name),
+								() => frm.set_value("last_name", trans.last_name),
 								() => done()
 							]);
 						}
@@ -74,6 +83,9 @@ frappe.ui.form.on("Journal Entry", {
 								() => frm.set_value("tin", trans.tin),
 								() => frm.set_value("address_1", trans.address_1),
 								() => frm.set_value("address_2", trans.address_2),
+								() => frm.set_value("first_name", trans.first_name),
+								() => frm.set_value("middle_name", trans.middle_name),
+								() => frm.set_value("last_name", trans.last_name),
 								() => done()
 							]);
 						}
